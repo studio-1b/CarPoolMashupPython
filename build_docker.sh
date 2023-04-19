42ba491c8447464fe9aeb711625c1eb96ad5b473
Bob on Linux Test <byuanbills@live.com>
2023-04-18 17:10:18 -0700

    fixed bugs to ask for google API keys, or search if they are in environment already

--- build_docker.sh
+++ build_docker.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 if [ "$(whoami)" != "root" ]; then
-   echo "This should be run as root.  Use sudo"
+   echo "This should be run as root.  Use sudo -E"
    echo "or run 'sudo usermod -aG docker $(whoami)'"
    exit
 fi
 
+
 grep "ENV GOOGLE_MAP_JS_API_KEY -" Docker/dockerfile > /dev/null
 if [ $? -eq 0 ]; then
-   if [ "$GOOGLE_MAP_JS_API_KEY" -ne "" ]; then
+   if [ "$GOOGLE_MAP_JS_API_KEY" != "" ]; then
       echo found the environment variable GOOGLE_MAP_JS_API_KEY
    else
       echo You have no Google API Key defined.  Get them from: 
+      echo https://developers.google.com/maps/documentation/javascript/get-api-key
       echo https://console.cloud.google.com/google/maps-apis/credentials
       echo if you need new keys, see \"CLI testdata script.txt\"
-      echo for which 4xAPI needs to be assigned to the key.
-      echo if you have the key, enter it now>
+      echo for which 4x API needs to be assigned to the key.
+      echo "if you have the key, enter it now(empty to abort):"
       read GOOGLE_MAP_JS_API_KEY
    fi
    if [ "$GOOGLE_MAP_JS_API_KEY" == "" ]; then
@@ -23,30 +25,34 @@ if [ $? -eq 0 ]; then
       exit
    else
       echo Applying GOOGLE_MAP_JS_API_KEY to dockerfile
-      sed -i "s/ENV GOOGLE_MAP_JS_API_KEY -/ENV GOOGLE_MAP_JS_API_KEY ${ENV GOOGLE_MAP_JS_API_KEY}/g" Docker/dockerfile
+      sed -i "s/ENV GOOGLE_MAP_JS_API_KEY -/ENV GOOGLE_MAP_JS_API_KEY ${GOOGLE_MAP_JS_API_KEY}/g" Docker/dockerfile
+      sleep 1
    fi
 fi
+
 grep "GOOGLE_GEOCODE_API_KEY -" Docker/dockerfile > /dev/null
 if [ $? -eq 0 ]; then
-   if [ "$GOOGLE_GEOCODE_API_KEY" -ne "" ]; then
+   if [ "$GOOGLE_GEOCODE_API_KEY" != "" ]; then
       echo found the environment variable GOOGLE_GEOCODE_API_KEY
-   elif [ "$GOOGLE_MAP_JS_API_KEY" -ne "" ]; then
+   elif [ "$GOOGLE_MAP_JS_API_KEY" != "" ]; then
       echo found the environment variable GOOGLE_MAP_JS_API_KEY
       echo and copying to GOOGLE_GEOCODE_API_KEY
       GOOGLE_GEOCODE_API_KEY=$GOOGLE_MAP_JS_API_KEY
    else
       echo You have no Google API Key defined.  Get them from: 
+      echo https://developers.google.com/maps/documentation/javascript/get-api-key
       echo https://console.cloud.google.com/google/maps-apis/credentials
       echo if you need new keys, see \"CLI testdata script.txt\"
-      echo for which 4xAPI needs to be assigned to the key.
-      echo if you have the key, enter it now>
+      echo for which 4x API needs to be assigned to the key.
+      echo "if you have the key, enter it now(empty to abort):"
       read GOOGLE_GEOCODE_API_KEY
    fi
    if [ "$GOOGLE_GEOCODE_API_KEY" == "" ]; then
       echo No API key, aborting Docker build
       exit
    else
-      sed -i "s/GOOGLE_GEOCODE_API_KEY -/GOOGLE_GEOCODE_API_KEY ${GOOGLE_GEOCODE_API_KEY}/g" Docker/dockerfile
+      sed -i "s/ENV GOOGLE_GEOCODE_API_KEY -/ENV GOOGLE_GEOCODE_API_KEY ${GOOGLE_GEOCODE_API_KEY}/g" Docker/dockerfile
+      sleep 1
    fi
 fi
 
